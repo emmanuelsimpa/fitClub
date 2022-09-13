2d6c8a10747dd737c1d7f95f6e7aecd6e1140c53
Bosey <bosey@Tosins-MBP.lan>
2022-09-13 10:07:57 +0100

    read me

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,9 @@ export default function Document() {
     <Html>
       <Head>
        {/* Favicon */}
-       <link rel="apple-touch-icon" sizes="180x180" href="../favicon_io/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="../favicon_io/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="../favicon_io/favicon-16x16.png"/>
+       <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png"/>
         <link rel="manifest" href="../favicon_io/site.webmanifest"/>
         {/* Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
